5f2be362fd4e6b5db461f4a8af3ab5185c67f7b8
남기문 <ngm011@naver.com>
2023-10-19 00:19:41 +0900

    Set Test 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+    }
+
+    @Test
+    void size(){
+        int cnt = numbers.size();
+        assertThat(cnt).isEqualTo(4);
+    }
+
+    @Test
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+        assertThat(numbers.contains(4)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +11,41 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split_1() {
+        String str = "1,2";
+        String[] strArr = str.split(",");
+        assertThat(strArr).contains("1");
+        assertThat(strArr).contains("2");
+    }
+
+    @Test
+    void split_2() {
+        String str = "1";
+        String[] strArr = str.split(",");
+        assertThat(strArr).containsExactly("1");
+
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String newStr = str.substring(1,str.length()-1);
+        assertThat(newStr).isEqualTo("1,2");
+    }
+
+    @Test
+    void StringIndexOutOfBoundsException() {
+        String str = "abc";
+
+        assertThatThrownBy(() -> {
+            str.charAt(6);
+        })
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessage("error");
+    }
+
+
+
 }
